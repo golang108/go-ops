e15524a692d137950a5686b7f18c55e1200e08c3
Lewis <shuaicode@gmail.com>
2022-04-27 18:27:48 +0800

    fix control

--- agent/action/agent_manager_control.go
+++ agent/action/agent_manager_control.go
@@ -8,14 +8,23 @@ import (
 )
 
 func (self *agentManager) Start(a *model.AgentInfo) (err error) {
+	workdir := self.agentDir + string(filepath.Separator) + a.Name
+	err = control(workdir, "start")
 	return
 }
 
 func (self *agentManager) Stop(a *model.AgentInfo) (err error) {
+	workdir := self.agentDir + string(filepath.Separator) + a.Name
+	err = control(workdir, "stop")
 	return
 }
 
 func (self *agentManager) Status(a *model.AgentInfo) (s string, err error) {
+	workdir := self.agentDir + string(filepath.Separator) + a.Name
+	err = control(workdir, "status")
+	if err != nil {
+		return
+	}
 	return
 }
 
@@ -24,6 +33,8 @@ func (self *agentManager) Version(a *model.AgentInfo) (s string, err error) {
 }
 
 func (self *agentManager) Delete(a *model.AgentInfo) (err error) {
+	workdir := self.agentDir + string(filepath.Separator) + a.Name
+	err = control(workdir, "delete")
 	return
 }
 
