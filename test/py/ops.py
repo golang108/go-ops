04b9143a3b263bc0b20d95faad4eb8ab758053ca
Lewis <shuaicode@gmail.com>
2022-05-14 00:29:47 -0700

    fix py, add signature

--- test/py/ops.py
+++ test/py/ops.py
@@ -1,38 +1,68 @@
 
+import base64
+import hashlib
+import hmac
 import json
+import uuid
 import requests
+import time
 
 
 class Ops:
+    apikey = ''
+    seckey = ''
     host = ''
+    appid = ''
 
-    def __init__(self, host, username='test', password='test'):
+    def __init__(self, host, appid, apikey, seckey):
         self.host = host
-        token = self.login({'username': username, 'passwd': password})
-        self.token = "Bearer " + token
+        self.appid = appid
+        self.apikey = apikey
+        self.seckey = seckey
+        
 
-    def post(self, address, data, isLogin=False):
-        url = self.host + address
-        dataStr = json.dumps(data)
+    def getSign(self, nonce, timestamp, body):
+        mac = hmac.new(bytes(self.seckey, encoding='utf-8'), None, hashlib.sha1)
+        mac.update(bytes(self.apikey, encoding='utf-8'))
+        mac.update(bytes(nonce, encoding='utf-8'))
+        mac.update(bytes(str(timestamp), encoding='utf-8'))
+        mac.update(bytes(body, encoding='utf-8'))
+        return base64.b64encode(mac.digest()).decode()
+
+    def request(self, method, url, data):
+        method = method.upper()
+        nonce = str(uuid.uuid1())
+        timestamp = str(int(time.time()))
+        body = json.dumps(data)
+        sign = self.getSign(nonce, timestamp, body)
 
         header = {
             'Content-Type': 'application/json',
+            'GO-OPS-X-TIMESTAMP': timestamp,
+            'GO-OPS-X-NONCE': nonce,
+            'GO-OPS-X-SIGNATURE': sign,
+            'GO-OPS-X-APPID': self.appid
         }
-        if not isLogin:
-            header['GO-OPS-X-TOKEN'] = self.token
-        res = requests.post(url=url, headers=header, data=dataStr)
-        return self.getData(res)
+        print("header:", header)
+        if method == 'POST':
+            return requests.post(url=url, headers=header, data=body)
+        elif method == 'PUT':
+            return requests.put(url=url, headers=header, data=body)
+        return {}
+
+
+    def post(self, address, data):
+        url = self.host + address
+        res = self.request('POST', url, data)
+        if res.status_code == 200:
+            return self.getData(res)
+        print("res:", res)
+        print("url:", url)
+        return {}
 
     def put(self, address, data, isLogin=False):
         url = self.host + address
-        dataStr = json.dumps(data)
-
-        header = {
-            'Content-Type': 'application/json'
-        }
-        if not isLogin:
-            header['GO-OPS-X-TOKEN'] = self.token
-        res = requests.put(url=url, headers=header, data=dataStr)
+        res = self.request('PUT', url, data)
         return self.getData(res)
 
     def login(self, data) -> str:
@@ -50,7 +80,7 @@ class Ops:
         return self.post("/peer/nodes", data)
 
     def script_add(self, data):
-        return self.post("/v1/m/script", data)
+        return self.post("/v1/m/script/add", data)
 
     def script_update(self, data):
         return self.put("/v1/m/script", data)
